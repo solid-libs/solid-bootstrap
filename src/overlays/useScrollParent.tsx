c61d4e214b6132c1e49582d9b3ba7332e621b392
Brendan <brendan@csel.co.nz>
2021-11-17 22:16:30 +1300

    add useScrollParent

---
+++ src/overlays/useScrollParent.tsx
@@ -0,0 +1,16 @@
+import getScrollParent from "dom-helpers/scrollParent";
+import { createEffect, createSignal } from "solid-js";
+
+export default function useScrollParent(element: () => null | HTMLElement) {
+  const [parent, setParent] = createSignal<
+    Document | HTMLElement | null | undefined
+  >(null);
+
+  createEffect(() => {
+    if (element()) {
+      setParent(() => getScrollParent(element()!, true));
+    }
+  });
+
+  return parent;
+}
