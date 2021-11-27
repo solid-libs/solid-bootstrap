6cc9d22f50ff24be943681feb3399c9b55f7e7af
Brendan <brendan@csel.co.nz>
2021-11-27 14:07:31 +1300

    Finish off Modal docs

--- src/docs/ModalDocs.tsx
+++ src/docs/ModalDocs.tsx
@@ -300,8 +300,35 @@ export const ModalDocs: Component = () => {
           </Card.Body>
         </Card>
         <Card>
-          <Card.Header></Card.Header>
-          <Card.Body></Card.Body>
+          <Card.Header>Sizing modals using custom CSS</Card.Header>
+          <Card.Body>
+            <Button variant="primary" onClick={handleOpen("custom")}>
+              Custom Width Modal
+            </Button>
+            <Modal
+              show={show("custom")}
+              onHide={handleClose("custom")}
+              dialogClassName="modal-90w"
+              aria-labelledby="example-custom-modal-styling-title"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="example-custom-modal-styling-title">
+                  Custom Modal Styling
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>
+                  Ipsum molestiae natus adipisci modi eligendi? Debitis amet
+                  quae unde commodi aspernatur enim, consectetur. Cumque
+                  deleniti temporibus ipsam atque a dolores quisquam quisquam
+                  adipisci possimus laboriosam. Quibusdam facilis doloribus
+                  debitis! Sit quasi quod accusamus eos quod. Ab quos
+                  consequuntur eaque quo rem! Mollitia reiciendis porro quo
+                  magni incidunt dolore amet atque facilis ipsum deleniti rem!
+                </p>
+              </Modal.Body>
+            </Modal>
+          </Card.Body>
         </Card>
       </div>
     </div>
--- src/docs/TabsDocs.tsx
+++ src/docs/TabsDocs.tsx
@@ -2,6 +2,9 @@ import { Component } from "solid-js";
 import Card from "../../packages/bootstrap/src/Card";
 import Tabs from "../../packages/bootstrap/src/Tabs";
 import Tab from "../../packages/bootstrap/src/Tab";
+import Row from "../../packages/bootstrap/src/Row";
+import Col from "../../packages/bootstrap/src/Col";
+import Nav from "../../packages/bootstrap/src/Nav";
 import Sonnet from "./Sonnet";
 
 export const TabsDocs: Component = () => {
@@ -33,16 +36,33 @@ export const TabsDocs: Component = () => {
           </Card.Body>
         </Card>
         <Card>
-          <Card.Header></Card.Header>
-          <Card.Body></Card.Body>
-        </Card>
-        <Card>
-          <Card.Header></Card.Header>
-          <Card.Body></Card.Body>
-        </Card>
-        <Card>
-          <Card.Header></Card.Header>
-          <Card.Body></Card.Body>
+          <Card.Header>Custom Tab Layout</Card.Header>
+          <Card.Body>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+              <Row>
+                <Col sm={3}>
+                  <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Col>
+                <Col sm={9}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                      <Sonnet />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <Sonnet />
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </Card.Body>
         </Card>
       </div>
     </div>
