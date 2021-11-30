cacc49498a99bda25d6b708157ee7f8bc788f789
Brendan <brendan@csel.co.nz>
2021-11-30 20:01:35 +1300

    Bump home page version

--- www/src/pages/Home.tsx
+++ www/src/pages/Home.tsx
@@ -30,7 +30,7 @@ const Home = () => (
             Components
           </Link>
         </div>
-        <div class="text-muted mt-3">Current version: 0.1.0</div>
+        <div class="text-muted mt-3">Current version: 0.1.1</div>
       </div>
     </div>
     <div class="container">
