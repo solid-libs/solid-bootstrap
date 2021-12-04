15df1bd2f95cf33932f1d950dcdf19035df12719
Brendan <brendan@csel.co.nz>
2021-12-04 19:52:13 +1300

    Refactor postbuild script

--- www/scripts/postbuild.js
+++ www/scripts/postbuild.js
@@ -1,74 +1,54 @@
+// Copies index.html for each route endpoint to workaround limitations
+// of static server like Github pages not supporting SPA routing
+
 const path = require("path");
 const fs = require("fs-extra");
 
 const docsPath = path.resolve(__dirname, "../../docs/");
-const componentsPath = path.resolve(__dirname, "../../docs/components");
-// This file is used by build system to copy index.html for each route endpoint
-// to workaround limitations of static server like Github pages not supporting SPA routing
+const componentsPath = path.join(docsPath, "components");
 
 function main() {
-  fs.copyFileSync(docsPath + "/index.html", docsPath + "/getting-started.html");
+  const source = docsPath + "/index.html";
+  // Root routes
+  fs.copyFileSync(source, path.join(docsPath, "getting-started.html"));
 
+  // Component routes
   fs.mkdirSync(componentsPath);
-
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/alerts.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/accordion.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/badge.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/breadcrumb.html"
-  );
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/buttons.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/button-group.html"
-  );
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/cards.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/carousel.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/close-button.html"
-  );
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/dropdowns.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/figures.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/forms.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/images.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/input-group.html"
-  );
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/list-group.html"
-  );
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/modal.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/navs.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/navbar.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/offcanvas.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/popovers.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/pagination.html"
-  );
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/placeholder.html"
-  );
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/progress.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/spinners.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/table.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/tooltips.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/toasts.html");
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/tabs.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/coreDropdown.html"
-  );
-  fs.copyFileSync(docsPath + "/index.html", componentsPath + "/coreModal.html");
-  fs.copyFileSync(
-    docsPath + "/index.html",
-    componentsPath + "/coreOverlay.html"
-  );
+  [
+    "alerts",
+    "accordion",
+    "badge",
+    "breadcrumb",
+    "buttons",
+    "button-group",
+    "cards",
+    "carousel",
+    "close-button",
+    "dropdowns",
+    "figures",
+    "forms",
+    "images",
+    "input-group",
+    "list-group",
+    "modal",
+    "navs",
+    "navbar",
+    "offcanvas",
+    "popovers",
+    "pagination",
+    "placeholder",
+    "progress",
+    "spinners",
+    "table",
+    "tooltips",
+    "toasts",
+    "tabs",
+    "coreDropdown",
+    "coreModal",
+    "coreOverlay",
+  ].forEach((dest) => {
+    fs.copyFileSync(source, path.join(componentsPath, `${dest}.html`));
+  });
 }
 
 main();
