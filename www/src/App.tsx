fa80786d3a9199aaa84369afc5d39cfc395a72fd
Brendan <brendan@csel.co.nz>
2022-12-05 22:47:59 +1300

    Docs - add suspense to reduce flash

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,5 +1,5 @@
 import {Route, Router, Routes} from "@solidjs/router";
-import {Component, lazy} from "solid-js";
+import {Component, lazy, Suspense} from "solid-js";
 import TopNavbar from "./layout/TopNavbar";
 import Home from "./pages/Home";
 import "./App.css";
@@ -9,14 +9,16 @@ const baseUrl = import.meta.env.BASE_URL;
 const App: Component = () => (
   <Router base={baseUrl}>
     <TopNavbar />
-    <Routes>
-      <Route path="/layout/*" component={lazy(() => import("./pages/Layouts"))} />
-      <Route path="/forms/*" component={lazy(() => import("./pages/Forms"))} />
-      <Route path="/components/*" component={lazy(() => import("./pages/Components"))} />
-      <Route path="/core/*" component={lazy(() => import("./pages/Core"))} />
-      <Route path="/getting-started" component={lazy(() => import("./pages/GettingStarted"))} />
-      <Route path="/*all" element={<Home />} />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path="/layout/*" component={lazy(() => import("./pages/Layouts"))} />
+        <Route path="/forms/*" component={lazy(() => import("./pages/Forms"))} />
+        <Route path="/components/*" component={lazy(() => import("./pages/Components"))} />
+        <Route path="/core/*" component={lazy(() => import("./pages/Core"))} />
+        <Route path="/getting-started" component={lazy(() => import("./pages/GettingStarted"))} />
+        <Route path="/*all" element={<Home />} />
+      </Routes>
+    </Suspense>
   </Router>
 );
 
